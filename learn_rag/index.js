b610af440f2474693c6a5032953276a2f2d4c3e1
neamatullah01 <neamat22205101908@diu.edu.bd>
2026-09-21 13:04:24 +0600

    feat: implemented a grocery store rag system with vector database

--- learn_rag/index.js
+++ learn_rag/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { ChatGroq } from "@langchain/groq";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { TaskType } from "@google/generative-ai";
 import dotenv from "dotenv";
 import fs from "fs";
 import { PDFParse } from "pdf-parse";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 dotenv.config();
 const app = express();
 const port = 5000;
@@ -15,7 +19,18 @@ const llm = new ChatGroq({
   temperature: 0.7,
   maxTokens: undefined,
   maxRetries: 2,
-  apiKey: process.env.GROQ_API_KEY,
+});
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  model: "gemini-embedding-001", // 768 dimensions
+  taskType: TaskType.RETRIEVAL_DOCUMENT,
+  title: "Document title",
+});
+
+const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY,
+  collectionName: "grocery-rag-testing",
 });
 
 const upload = async () => {
@@ -27,23 +42,32 @@ const upload = async () => {
     chunkOverlap: 200,
   });
   const docs = await splitter.createDocuments([text]);
-  console.log(docs);
+  const res = await vectorStore.addDocuments(docs);
 };
 
 app.post("/ai", async (req, res) => {
   const { input } = req.body;
 
-  const response = await graph.invoke({
-    messages: [
-      {
-        role: "user",
-        content: input,
-      },
-    ],
-  });
-  return res
-    .status(200)
-    .json({ "ai:": response.messages[response.messages.length - 1].content });
+  const docs = await vectorStore.similaritySearch(input, 5);
+  const context = docs.map((d) => d.pageContent).join("/n");
+
+  const response = await llm.invoke([
+    new SystemMessage(`You are a RAG AI assistant.
+
+STRICT RULES:
+- Answer ONLY from context
+- Do not use outside knowledge
+- If answer not found say:
+  "I don't know from uploaded PDF."
+
+Context:
+${context}`),
+    new HumanMessage(input),
+  ]);
+
+  console.log(response);
+
+  return res.status(200).json({ ai: response.content });
 });
 
 app.get("/", (req, res) => {
