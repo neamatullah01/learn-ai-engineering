1406a2d1047179f35d80fedc49f7008a2efc4719
neamatullah01 <neamat22205101908@diu.edu.bd>
2026-09-21 12:24:11 +0600

    feat: read file and creates chunks implemented

--- learn_rag/index.js
+++ learn_rag/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { ChatGroq } from "@langchain/groq";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import dotenv from "dotenv";
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
 dotenv.config();
 const app = express();
 const port = 5000;
@@ -14,6 +17,18 @@ const llm = new ChatGroq({
   maxRetries: 2,
   apiKey: process.env.GROQ_API_KEY,
 });
+
+const upload = async () => {
+  const pdfPath = "./knowledge.pdf";
+  const buffer = await fs.readFileSync(pdfPath);
+  const text = (await new PDFParse({ data: buffer }).getText()).text;
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+  const docs = await splitter.createDocuments([text]);
+  console.log(docs);
+};
 
 app.post("/ai", async (req, res) => {
   const { input } = req.body;
